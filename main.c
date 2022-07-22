87dc3bc24a9084c189bb281ec8b678070e8ce840
Luiz Brito da Rosa <luizbrt989@gmail.com>
2022-07-21 23:21:37 -0300

    limpeza do código

--- main.c
+++ main.c
@@ -19,9 +19,7 @@
 #define digit4 PORTAbits.RA3
 
 char num[10]={0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
-
-unsigned int m, c, d, u, i;
-
+ 
 void inicializaParam(void){
     TRISA = 0b0;
     TRISB = 0b10000000;
@@ -32,60 +30,6 @@ void inicializaParam(void){
     digit2=0; 
     digit3=0;
     digit4=0;
-}
-
-inicializaSegmento(void){
-    i = 0;
-    while (i < 1001) {
-       
-        mostraUnidade(i);
-        mostraDezena(d);
-        mostraCentena(c);        
-        mostraMilhar(m);  
-        __delay_ms(100);        
-    }
-}
-
-mostraUnidade(int v){      
-     u = (v%10);
-     PORTAbits.RA0 = 1; 
-     seg_port=num[u];
-     __delay_ms(1);
-     PORTAbits.RA0 = 0;
-     PORTB = 0;
-     __delay_ms(1);
-}
-
-mostraDezena(int v){
-    d = (v%100);
-    d = (d/10)-((d%10)/10);
-    PORTAbits.RA1 = 1; 
-    seg_port=num[d];
-    __delay_ms(1);
-    PORTAbits.RA1 = 0;
-    PORTB = 0;
-    __delay_ms(1);
-}
-
-mostraCentena(int v){ 
-    c = (v%1000);
-    c = (c/100)-((c%100)/100);
-    PORTAbits.RA2 = 1; 
-    seg_port=num[c];
-    __delay_ms(1);
-    PORTAbits.RA2 = 0;
-    PORTB = 0;
-    __delay_ms(1);
-}
-
-mostraMilhar(int v){ 
-    m = (v/1000)-((v%1000)/1000);
-    PORTAbits.RA3 = 1; 
-    seg_port=num[v];
-    __delay_ms(1);
-    PORTAbits.RA3 = 0;
-    PORTB = 0;
-    __delay_ms(1);
 }
 
 mostraAniversario(void){
@@ -116,7 +60,5 @@ void main(void) {
     int w = 0;    
     while (1){
         w = mostraAniversario();    
-    }
-    // seguir testar. 
-    // inicializaSegmento();
+    } 
 }
